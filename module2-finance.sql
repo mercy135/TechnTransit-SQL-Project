12b7ae0b11dbf0730801f5c31ab8d27016ac3a85
mercy135 <42065455+mercy135@users.noreply.github.com>
2024-08-13 10:42:01 +0100

    Update module2-finance.sql

--- module2-finance.sql
+++ module2-finance.sql
@@ -1,2 +1,8 @@
-*//The finance team needs a report of all orders along with their respective customers'
+/*The finance team needs a report of all orders along with their respective customers'
 contact details. How would you retrieve this data using the orders and customers tables?*/
+
+SELECT o.orderNumber, o.orderDate, o.status, 
+c.contactLastName,c.contactFirstName,c.phone
+FROM orders o
+INNER JOIN customers c
+ON o.customerNumber=c.customerNumber;
