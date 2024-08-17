3ced27a6e1e25ee6a837e186ce4902da69c58755
mercy135 <42065455+mercy135@users.noreply.github.com>
2024-08-17 16:48:41 +0100

    Update module1-sales.sql

--- module1-sales.sql
+++ module1-sales.sql
@@ -9,3 +9,14 @@ GROUP BY customerNumber
 ORDER BY TotalAmount DESC
 LIMIT 5;
 
+
+
+/*The sales team wants to track the shipment status of orders along with the product
+details for each order. How would you get this data by joining the orders, orderdetails, and
+products tables?*/
+
+SELECT o.status, p.productCode, p.productName
+FROM orders o
+INNER JOIN orderdetails od ON o.orderNumber = od.orderNumber
+INNER JOIN products p ON od.productCode = p.productCode
+GROUP BY p.productCode;
